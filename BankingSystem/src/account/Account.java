956a43e03b1ec929e14fc56a9ba9c9b2f4f50b11
songstone <thdtmdgus5505@gmail.com>
2021-08-27 22:03:57 +0900

    fix: delete some annotation(TODO, guide)
    delete some annotation(TODO, guide)

--- BankingSystem/src/account/Account.java
+++ BankingSystem/src/account/Account.java
@@ -5,20 +5,13 @@ import bank.Bank;
 import java.math.BigDecimal;
 
 public class Account {
-    //TODO: 일반 계좌 클래스의 속성은 계좌종류(N: 예금계좌, S:적금계좌), 계좌번호, 소유자, 잔액, 활성화 여부 5가지 입니다.
-    //category;             // String 계좌 종류
     private String category;
-    // accNo;               // String 계좌번호
     private String accNo;
-    // owner;               // String 소유자
     private String owner;
-    // balance;             // BigDecimal 잔액
     private BigDecimal balance;
-    // isActive;            // boolean 활성화 여부
     private boolean isActive;
 
     public Account() {
-        //TODO: 일반 계좌의 활성화 여부를 True로, 계좌 종류를 "N"(NORMAL을 의미) 설정해줍니다.
         this.category = "N";
         this.isActive = true;
     }
@@ -30,8 +23,6 @@ public class Account {
         this.balance = balance;
         this.isActive = true;
     }
-
-    // TODO: 일반 계좌 클래스의 각 속성에 getter/setter를 제공합니다.
 
     public String getCategory() {
         return category;
@@ -74,18 +65,14 @@ public class Account {
     }
 
     public void getAccountInfo(Account account){
-        //TODO: 계좌의 기본 정보를 아래 형태로 출력해줍니다.
         System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원\n",category,accNo,owner,balance);
-        //계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원
     }
 
     public BigDecimal withdraw(BigDecimal amount) throws Exception{
-        //TODO: 출금액을 받아서 출금하는 기본 메소드입니다. this를 이용해 구현해보세요.
         this.balance = this.balance.subtract(amount);
         return amount;
     }
     public BigDecimal deposit(BigDecimal amount){
-        //TODO: 입금액을 받아서 입금하는 기본 메소드입니다. this를 이용해 구현해보세요.
         this.balance = this.balance.add(amount);
         return amount;
     }
