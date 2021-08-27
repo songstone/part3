bdb0524272ca32c8a76cc479e9864f07bb6d8a58
songstone <thdtmdgus5505@gmail.com>
2021-08-27 21:29:32 +0900

    feat: complete getAccountInfo method
    - complete getAccountInfo method to print account information.

--- BankingSystem/src/account/Account.java
+++ BankingSystem/src/account/Account.java
@@ -75,6 +75,7 @@ public class Account {
 
     public void getAccountInfo(Account account){
         //TODO: 계좌의 기본 정보를 아래 형태로 출력해줍니다.
+        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원\n",category,accNo,owner,balance);
         //계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원
     }
 
