63d8f8ac8b295980ef3355aacd662abcf4b11b33
songstone <thdtmdgus5505@gmail.com>
2021-08-29 14:21:41 +0900

    fest: define getInterest method
    - define getInterest method

--- BankingSystem/src/bank/InterestCalculator.java
+++ BankingSystem/src/bank/InterestCalculator.java
@@ -4,4 +4,5 @@ import java.math.BigDecimal;
 
 public interface InterestCalculator {
     //TODO: 잔액에 대한 이자 금액을 반환하는 메서드 ( BigDecimal getInterest(balance))를 선언합니다.
+    BigDecimal getInterest(BigDecimal balance);
 }
