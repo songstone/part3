e487ca4fbed153d66b124e38c99d5744b98f92e3
songstone <thdtmdgus5505@gmail.com>
2021-08-29 17:22:52 +0900

    feat: implement savingAccount withdraw method
    - implement savingAccount withdraw method basic logic

--- BankingSystem/src/bank/SavingBank.java
+++ BankingSystem/src/bank/SavingBank.java
@@ -11,6 +11,19 @@ public class SavingBank extends Bank {
     public void withdraw(SavingAccount account) throws Exception{
         // TODO: Account의 출금 메서드에서 잔액/목표 금액 체크하여 조금 다르게 구현
         // throws Exception 적금 계좌는 잔액이 목표 금액(%s원) 이상이어야 출금 가능합니다.
+        BigDecimal balance = account.getBalance();
+        BigDecimal goalAmount = account.getGoalAmount();
+        if(balance.compareTo(goalAmount)>=0){
+            System.out.println("\n출금할 금액을 입력하세요.");
+            String amount = scanner.next();
+
+            BigDecimal interest = interestCalculators.get("S").getInterest(balance);
+            BigDecimal withdrawAmount = account.withdraw(new BigDecimal(amount)).add(interest);
+
+            System.out.println("\n이자 "+ df.format(interest) + "원을 포함한 " + df.format(withdrawAmount) + "원이 출금되었습니다.");
+        }
+        else
+            System.out.printf("\n적금 계좌는 잔액(%s)이 목표 금액(%s) 이상이어야 출금 가능합니다.\n",df.format(balance),df.format(goalAmount));
     }
     // TODO: 목표금액을 입력받아서 SavingAccount 객체 생성하도록 재정의
     @Override
